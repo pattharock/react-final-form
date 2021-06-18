4d0feb237ee81daafae9a520fdf4e86971a35de1
pattharock <ritvik1609@gmail.com>
2021-06-18 11:51:19 +0800

    added <Field/>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import {Form, Field} from 'react-final-form'
+import { Form, Field } from 'react-final-form';
 import './App.css';
 
 function App() {
+  const onSubmit = (e) => {
+    debugger;
+  };
+
+  const validate  = (e) => {
+    debugger;
+  };
+
   return (
-    <div className="App">
-      <header  eader className="App-header">
+    <div className='App'>
+      <header eader className='App-header'>
         <Form
           onSubmit={onSubmit}
           validate={validate}
-          render = {({handleSubmit}) => (
-            
+          render={({ handleSubmit }) => (
+            <form onSubmit={handleSumbit}>
+              <div>
+                <label> First Name </label>
+                <Field
+                  name='firstName'
+                  component='input'
+                  placeholder='First Name'
+                />
+              </div>
+              <button type='submit'> Submit </button>
+            </form>
           )}
         />
       </header>
